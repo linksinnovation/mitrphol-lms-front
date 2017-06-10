540c91a6b418866af843719ba2d0bd500ca9429b
Jirawong Wongdokpuang <greannetwork@gmail.com>
2017-06-11 05:30:03 +0700

    initial with material design

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="ms-preload" class="ms-preload">
+      <div id="status">
+        <div class="spinner">
+          <div class="dot1"></div>
+          <div class="dot2"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <div class="middle-box text-center animated fadeInDown">
+      <div>
+        <div>
+          <h1 class="logo-name">INTEGRITY</h1>
+        </div>
+        <h3>ASSESSMENT</h3>
+        <form class="mt" role="form" v-on:submit.prevent="onSubmit" autocomplete="off">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username" required="" v-model="credentials.username">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" required="" v-model="credentials.password">
+          </div>
+          <button type="submit" class="btn btn-raised btn-primary block full-width mb">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function () {
+    return {
+      credentials: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
